f96110eb08d7470f520bef7c8efe79d862d9f891
Adivinedude <151443912+Adivinedude@users.noreply.github.com>
2023-12-21 19:45:58 -0500

    Add files via upload
    added rtu connection options

--- menus/menu_connections.cpp
+++ menus/menu_connections.cpp
@@ -12,6 +12,7 @@ SerialHandler _serial;
 void ConnectRTU(const uint16_t value);
 void ConnectTCP(const uint16_t value);
 void Disconnect(const uint16_t value);
+void ConnectionOptions(const uint16_t value);
 
 extern bool restart;
 
@@ -27,16 +28,16 @@ void QuitProgram(const uint16_t value) {
     DoExit(0);
     restart = 0;
 }
-
 void Menu_Callback_Connect(const uint16_t value)
 {
     //enable/disable menu options
     menu SubMenuConnect[] =
     {
-        { "Connect/RTU",ConnectRTU, "Establish a Modbus RTU Connection"},
-        { "Connect/TCP",ConnectTCP, "Establish a Modbus TCP Connection"},
-        { "Disconnect", Disconnect, "Close Connection" },
-        { "Exit",       QuitProgram, "Terminate program" },
+        { "Connect/RTU",    ConnectRTU,         "Establish a Modbus RTU Connection"},
+        { "RTU Options",    ConnectionOptions,  "Configure RTU" },
+        { "Connect/TCP",    ConnectTCP,         "Establish a Modbus TCP Connection"},
+        { "Disconnect",     Disconnect,         "Close Connection" },
+        { "Exit",           QuitProgram,        "Terminate program" },
         END_OF_MENU
     };
     
@@ -45,17 +46,18 @@ void Menu_Callback_Connect(const uint16_t value)
     if (_gIsConnected) {
         SubMenuConnect[0].value = MENU_DISABLE;
         SubMenuConnect[1].value = MENU_DISABLE;
-        SubMenuConnect[2].value = 0x00;
+        SubMenuConnect[2].value = MENU_DISABLE;
+        SubMenuConnect[3].value = 0x00;
     }
     else {
         SubMenuConnect[0].value = 0x00;
         SubMenuConnect[1].value = 0x00;
-        SubMenuConnect[2].value = MENU_DISABLE;
+        SubMenuConnect[2].value = 0x00;
+        SubMenuConnect[3].value = MENU_DISABLE;
     }
     SubMenuConnectState.hotkey = true;
     domenu(&SubMenuConnectState);
 }
-
 void ConnectRTUSubmenu(const uint16_t value);
 //build another sub menu populated with com ports
 void ConnectRTU(const uint16_t value) {
@@ -79,7 +81,6 @@ void ConnectRTU(const uint16_t value) {
         delete[] ms.pMenu;
     }
 }
-
 void ConnectRTUSubmenu(const uint16_t value) {
     key = KEY_ESC;
     //easier and cleaner way to do this. ToDo properly use this vector
@@ -102,11 +103,9 @@ void ConnectRTUSubmenu(const uint16_t value) {
 
     connect_poller( temp.c_str() );
 }
-
 void ConnectTCP(const uint16_t value) {
     NotYetImplemented(0);
 }
-
 void Disconnect(const uint16_t value) {
     if (_gIsConnected) {
         stop_poller();
@@ -117,4 +116,85 @@ void Disconnect(const uint16_t value) {
         statusmsg("Disconnected");
     }
     key = KEY_ESC;
+}
+
+enum connection_options_enum {
+    baud = 0,
+    parity,
+    data_bit,
+    stop_bit
+};
+
+bool ProcessData_ConnectionOptions(char** fieldbuf, size_t fieldsize) {
+    sConnection_Options o;
+    try {
+        o.baudrate  = std::stoi( fieldbuf[baud], nullptr, 10);
+        o.parity    = fieldbuf[parity][0];
+        o.data_bit  = std::stoi( fieldbuf[data_bit], nullptr, 10);
+        o.stop_bit  = std::stoi( fieldbuf[stop_bit], nullptr, 10);
+
+        //error check the user input
+        switch (o.baudrate) {
+            case 110:
+            case 300:
+            case 600:
+            case 1200:
+            case 2400:
+            case 4800:
+            case 9600:
+            case 14400:
+            case 19200:
+            case 38400:
+            case 57600:
+            case 115200:
+            case 128000:
+            case 256000:
+                break;
+            default:
+                std::cerr << "Invalid Baudrate" << std::endl;
+                return true;
+        }
+        switch (o.parity) {
+            case 'E':
+            case 'e':
+            case 'N':
+            case 'n':
+            case 'O':
+            case 'o':
+                break;
+            default:
+                std::cerr << "Invalid parity" << std::endl;
+                return true;
+        }
+        if (o.data_bit < 5 || o.data_bit > 8) {
+                std::cerr << "Invalid data bit" << std::endl;
+                return true;
+        }
+        if (o.stop_bit < 1 || o.stop_bit > 2) {
+            std::cerr << "Invalid stop bit" << std::endl;
+            return true;
+        }
+        Current_Connection_Settings.baudrate    = o.baudrate;
+        Current_Connection_Settings.parity      = o.parity;
+        Current_Connection_Settings.data_bit    = o.data_bit;
+        Current_Connection_Settings.stop_bit    = o.stop_bit;
+
+        return false;
+    }
+    catch (std::invalid_argument const& ex) {
+        errormsg(ex.what());
+    }
+    return true;
+}
+void ConnectionOptions(const uint16_t value)
+{
+    const char* fieldname[] = { "Baudrate:", "Parity:", "Data bits:", "Stop bits", 0 };
+    std::string b, p, d, s;
+    b = std::to_string(Current_Connection_Settings.baudrate);
+    p = Current_Connection_Settings.parity;
+    d = std::to_string(Current_Connection_Settings.data_bit);
+    s = std::to_string(Current_Connection_Settings.stop_bit);
+    const char* defaultfields[] = { b.c_str(), p.c_str(), d.c_str(), s.c_str(), 0};
+
+    TextInputBox_Fields(fieldname, DEFAULT_FIELD_LENGTH, ProcessData_ConnectionOptions, defaultfields);
 }
